f5049daba8533e6e951e203f6a3145b71be426e8
SamFunky <115422578+SamFunky@users.noreply.github.com>
2025-02-07 16:49:32 -0700

    Revert "Add multiplayer plugins"
    This reverts commit d7bb87d3c443f2f77f9a305789c7197639ebd4ac.

--- Game02_2/Source/Game02_2/Game02_2.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Game02_2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game02_2, "Game02_2" );
